915e55e97f7fcfb3b3ec9ca5635db1d7e13cdc64
Ethan <liao1ping@gmail.com>
2017-06-29 18:10:47 -0500

    heroku deploy

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://pqoklsdrxgnznq:83c1ccb52beccb22ba079846358691d9dc811d767b2adb82815923439952522e@ec2-184-73-167-43.compute-1.amazonaws.com:5432/dev2ah3rtjn5m6',
+    connection: 'postgres://rketwcgabobqtt:f2b1e991138609d6a45f8d02a937226a620526913c1dc7fd410d6a4e9b3e15cb@ec2-184-73-249-56.compute-1.amazonaws.com:5432/d78amgf03bor69',
     migrations: {
       directory: __dirname + '/public/server/db/migrations'
     },
